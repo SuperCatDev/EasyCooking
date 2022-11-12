9d26fe4059540d87471c86a40db8beba0b9e1fa0
Nikita Timchenko <n.timchenko@vk.team>
2022-11-12 22:09:36 +0300

    Added things for settings (setting rows and SwitchLight)

---
+++ core/view_ext/src/main/java/com/sc/easycooking/view_ext/views/SwitchLight.kt
@@ -0,0 +1,81 @@
+package com.sc.easycooking.view_ext.views
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SwitchLight(
+    modifier: Modifier,
+    checked: Boolean,
+    width: Dp = 36.dp,
+    height: Dp = 20.dp,
+    strokeWidth: Dp = 2.dp,
+    checkedTrackColor: Color = MaterialTheme.colorScheme.primary,
+    uncheckedTrackColor: Color = MaterialTheme.colorScheme.secondary.copy(alpha = 0.5f),
+    gapBetweenThumbAndTrackEdge: Dp = 4.dp,
+    checkChanged: (Boolean) -> Unit,
+) {
+
+    val switchON = remember {
+        mutableStateOf(checked) // Initially the switch is ON
+    }
+
+    val thumbRadius = (height / 2) - gapBetweenThumbAndTrackEdge
+
+    // To move thumb, we need to calculate the position (along x axis)
+    val animatePosition = animateFloatAsState(
+        targetValue = if (switchON.value)
+            with(LocalDensity.current) { (width - thumbRadius - gapBetweenThumbAndTrackEdge).toPx() }
+        else
+            with(LocalDensity.current) { (thumbRadius + gapBetweenThumbAndTrackEdge).toPx() }
+    )
+
+    Canvas(
+        modifier = modifier
+            .size(width = width, height = height)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        // This is called when the user taps on the canvas
+                        switchON.value = !switchON.value
+                    }
+                )
+            }
+    ) {
+        // Track
+        drawRoundRect(
+            color = if (switchON.value) checkedTrackColor else uncheckedTrackColor,
+            cornerRadius = CornerRadius(x = 10.dp.toPx(), y = 10.dp.toPx()),
+            style = Stroke(width = strokeWidth.toPx())
+        )
+
+        // Thumb
+        drawCircle(
+            color = if (switchON.value) checkedTrackColor else uncheckedTrackColor,
+            radius = thumbRadius.toPx(),
+            center = Offset(
+                x = animatePosition.value,
+                y = size.height / 2
+            )
+        )
+    }
+
+    Spacer(modifier = Modifier.height(18.dp))
+}
--- settings-impl/src/main/java/com/sc/easycooking/settings/impl/ui/SettingsScreen.kt
+++ settings-impl/src/main/java/com/sc/easycooking/settings/impl/ui/SettingsScreen.kt
@@ -1,15 +1,40 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.sc.easycooking.settings.impl.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.outlinedButtonColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.pinnedScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.sc.easycooking.view_ext.insets.WrapWithColoredSystemBars
+import com.sc.easycooking.view_ext.views.SwitchLight
 
 @Composable
 internal fun SettingsScreenRoute(
@@ -19,6 +44,7 @@ internal fun SettingsScreenRoute(
     SettingsScreen(modifier, onBackClick)
 }
 
+@Suppress("SameParameterValue")
 @Composable
 internal fun SettingsScreen(
     modifier: Modifier = Modifier,
@@ -28,12 +54,103 @@ internal fun SettingsScreen(
         modifier = modifier,
         navBarColor = null,
     ) { innerModifier ->
-        Box(
+
+        val checkedSwitch = remember {
+            mutableStateOf(false)
+        }
+
+        Scaffold(
             modifier = innerModifier
                 .fillMaxSize()
-                .padding(WindowInsets.systemBars.asPaddingValues())
-        ) {
-            Text(text = "Hello world!")
-        }
+                .padding(WindowInsets.systemBars.asPaddingValues()),
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            modifier = Modifier,
+                            text = "Settings"
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = onBackClick) {
+                            Icon(Icons.Default.ArrowBack, contentDescription = "Settings")
+                        }
+                    },
+                    scrollBehavior = pinnedScrollBehavior()
+                )
+            },
+            content = { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        SettingsBlock(blockTitle = "Отображение") {
+
+                            Button(
+                                onClick = { },
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = outlinedButtonColors(),
+                                contentPadding = PaddingValues(horizontal = 16.dp),
+                                shape = ShapeDefaults.Medium
+                            ) {
+                                Text(
+                                    modifier = Modifier.weight(1f),
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    text = "Тема"
+                                )
+
+                                Text(
+                                    modifier = Modifier.weight(1f),
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    textAlign = TextAlign.End,
+                                    text = "По умолчанию",
+                                )
+                            }
+
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 16.dp)
+                            ) {
+                                Text(
+                                    modifier = Modifier.align(Alignment.CenterStart),
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = MaterialTheme.colorScheme.onBackground,
+                                    text = "Material you"
+                                )
+
+                                SwitchLight(
+                                    modifier = Modifier.align(Alignment.CenterEnd),
+                                    checked = false,
+                                ) { _ ->
+
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Suppress("SameParameterValue")
+@Composable
+private inline fun SettingsBlock(
+    modifier: Modifier = Modifier,
+    blockTitle: String,
+    block: @Composable ColumnScope.() -> Unit
+) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Text(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            text = blockTitle,
+            style = MaterialTheme.typography.titleSmall,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        block.invoke(this)
     }
 }
