84e987aca061db60244712a3225633861b2720aa
Nikita Timchenko <n.timchenko@vk.team>
2022-11-20 20:33:38 +0300

    Simple refactoring

---
+++ recipes-impl/src/main/java/com/sc/easycooking/recipes/impl/ui/RecipeItem.kt
@@ -0,0 +1,121 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.sc.easycooking.recipes.impl.ui
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.sc.easycooking.recipes.impl.R
+import com.sc.easycooking.recipes.impl.presentation.RecipesListViewModel
+import com.sc.easycooking.recipes.impl.presentation.models.RecipeUiModelShort
+import com.sc.easycooking.recipes.impl.ui.hashtag.HashTagRow
+
+@Composable
+internal fun RecipeItem(
+    item: RecipeUiModelShort?,
+    selectedItems: Set<RecipeUiModelShort>,
+    viewModel: RecipesListViewModel,
+) {
+    val selected = selectedItems.contains(item)
+    val hapticFeedback = LocalHapticFeedback.current
+    val roundSize = 12.dp
+
+    Box(
+        modifier = Modifier
+            .shadow(
+                elevation = if (selected) 5.dp else 0.dp,
+                shape = RoundedCornerShape(roundSize),
+            )
+            .background(
+                color = MaterialTheme.colorScheme.background,
+                shape = RoundedCornerShape(roundSize),
+            )
+            .border(
+                width = if (selected) 2.dp else 1.dp,
+                color = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground,
+                shape = RoundedCornerShape(roundSize),
+            )
+            .clip(RoundedCornerShape(roundSize))
+            .combinedClickable(
+                enabled = item != null,
+                onClick = {
+                    item?.let {
+                        viewModel.clickedAt(it)
+                    }
+                },
+                onLongClick = {
+                    hapticFeedback.performHapticFeedback(
+                        HapticFeedbackType.LongPress
+                    )
+                    item?.let {
+                        viewModel.longClickedAt(it)
+                    }
+                }
+            ),
+        contentAlignment = Alignment.TopStart,
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            if (item != null) {
+                Text(
+                    text = item.name,
+                    modifier = Modifier.align(Alignment.Start),
+                    style = MaterialTheme.typography.titleMedium
+                )
+                item.ingredients.forEachIndexed { index, ingredient ->
+                    Spacer(modifier = Modifier.height(if (index == 0) 16.dp else 2.dp))
+
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        Text(
+                            text = ingredient.name,
+                            modifier = Modifier.weight(1f),
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+
+                        Text(
+                            text = "${ingredient.amount} ${ingredient.quantity}",
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.End,
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                    }
+
+                }
+            } else {
+                Text(
+                    text = stringResource(id = R.string.recipe_item_loading),
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+
+            if (item?.tags?.isNotEmpty() == true) {
+                Spacer(modifier = Modifier.padding(top = 8.dp))
+                HashTagRow(tags = item.tags.map { it.nameId })
+            }
+        }
+    }
+}
--- recipes-impl/src/main/java/com/sc/easycooking/recipes/impl/ui/RecipesListScreen.kt
+++ recipes-impl/src/main/java/com/sc/easycooking/recipes/impl/ui/RecipesListScreen.kt
@@ -11,20 +11,12 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumedWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyStaggeredGridScope
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
@@ -56,16 +48,11 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
@@ -78,8 +65,6 @@ import com.example.view_ext.anims.scaleOutDuration
 import com.sc.easycooking.recipes.impl.R
 import com.sc.easycooking.recipes.impl.presentation.RecipesListViewModel
 import com.sc.easycooking.recipes.impl.presentation.models.RecipeUiModelShort
-import com.sc.easycooking.recipes.impl.presentation.models.RecipeUiTag
-import com.sc.easycooking.recipes.impl.ui.hashtag.HashTagRow
 import com.sc.easycooking.recipes.impl.ui.paging.items
 import com.sc.easycooking.view_ext.insets.WrapWithColoredSystemBars
 
@@ -164,87 +149,7 @@ private fun LazyStaggeredGridScope.recipeListScreen(
 ) {
 
     items(lazyItems, key = { it.id }) { item: RecipeUiModelShort? ->
-        val selected = selectedItems.contains(item)
-        val hapticFeedback = LocalHapticFeedback.current
-        val roundSize = 12.dp
-
-        Box(
-            modifier = Modifier
-                .shadow(
-                    elevation = if (selected) 5.dp else 0.dp,
-                    shape = RoundedCornerShape(roundSize),
-                )
-                .background(
-                    color = MaterialTheme.colorScheme.background,
-                    shape = RoundedCornerShape(roundSize),
-                )
-                .border(
-                    width = if (selected) 2.dp else 1.dp,
-                    color = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground,
-                    shape = RoundedCornerShape(roundSize),
-                )
-                .clip(RoundedCornerShape(roundSize))
-                .combinedClickable(
-                    enabled = item != null,
-                    onClick = {
-                        item?.let {
-                            viewModel.clickedAt(it)
-                        }
-                    },
-                    onLongClick = {
-                        hapticFeedback.performHapticFeedback(
-                            HapticFeedbackType.LongPress
-                        )
-                        item?.let {
-                            viewModel.longClickedAt(it)
-                        }
-                    }
-                ),
-            contentAlignment = Alignment.TopStart,
-        ) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-            ) {
-                if (item != null) {
-                    Text(
-                        text = item.name,
-                        modifier = Modifier.align(Alignment.Start),
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    item.ingredients.forEachIndexed { index, ingredient ->
-                        Spacer(modifier = Modifier.height(if (index == 0) 16.dp else 2.dp))
-
-                        Row(modifier = Modifier.fillMaxWidth()) {
-                            Text(
-                                text = ingredient.name,
-                                modifier = Modifier.weight(1f),
-                                style = MaterialTheme.typography.bodySmall,
-                            )
-
-                            Text(
-                                text = "${ingredient.amount} ${ingredient.quantity}",
-                                modifier = Modifier.weight(1f),
-                                textAlign = TextAlign.End,
-                                style = MaterialTheme.typography.bodySmall,
-                            )
-                        }
-
-                    }
-                } else {
-                    Text(
-                        text = stringResource(id = R.string.recipe_item_loading),
-                        modifier = Modifier.align(Alignment.CenterHorizontally),
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-
-                if (item?.tags?.isNotEmpty() == true) {
-                    Spacer(modifier = Modifier.padding(top = 8.dp))
-                    HashTagRow(tags = item.tags.map { it.nameId })
-                }
-            }
-        }
+        RecipeItem(item, selectedItems, viewModel)
     }
 }
 
